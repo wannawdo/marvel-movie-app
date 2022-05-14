9b17c6c02c09f3dc45bcfb8ffd05c97f34ff5839
Oana <wannawdo@gmail.com>
2022-05-14 23:35:23 +0300

    updated package.json

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AppHeader from "./components/AppHeader";
 import Box from "@mui/material/Box";
 
 function App() {
+  
+  const apiKey = process.env.REACT_APP_API_KEY;
   //movieUrl=https://api.themoviedb.org/3/discover/movie?api_key=51b3258aa0f7c5db9e22bb77f666162f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_keywords=180547
   const movieData = {
     page: 1,
